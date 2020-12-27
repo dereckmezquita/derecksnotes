e4160bc8fd2c6b43cfc92a18f6de282fdcfae849
dereckdemezquita <dereck@demezquita.com>
2020-12-27 03:00:31 -0800

    Articles/books

---
+++ references/articlesBooks/articlesBooks.php
@@ -0,0 +1,31 @@
+<html lang="en-uk">
+    <head>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/includes/header.php"; ?>
+        <script type="text/javascript" src="/references/articlesBooks/getEntries.js"></script>
+        <title>Books</title>
+    </head>
+
+    <body>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/includes/nav.php"; ?>
+
+            <div class="contentWrapper">
+                <div class="writtenWrapper">
+                    <h1>Articles & Books</h1>
+                        <p>
+                            Here are various articles (scientific or not) & books that I've read and found useful. If I am distrubuting a copyrighted work and you would like to see your worked removed from this page, please let me know and I will remove it. I hope these are accessible to all and useful for those in need.
+                        </p>
+                        <p>
+                            When possible and of value, I try to bring some commentary, or notes on the subject to better help understand. In some cases I'll try to summurise and simply present the work in a few lines for quick consumption.
+                        </p>
+                </div>
+            </div>
+
+            <div class="wrapper">
+                <div class="entries">
+
+                </div>
+            </div>
+    </body>
+
+    <?php include $_SERVER['DOCUMENT_ROOT'] . "/includes/footer.php"; ?>
+</html>
