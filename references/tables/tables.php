4a7cb7b7853d72683cd422d73cfc569bb878e8b7
dereckdemezquita <dereck@demezquita.com>
2020-12-27 03:04:33 -0800

    Tables.

---
+++ references/tables/tables.php
@@ -0,0 +1,28 @@
+<html lang="en-uk">
+    <head>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/includes/header.php"; ?>
+        <script type="text/javascript" src="/references/tables/getEntries.js"></script>
+        <title>Reference tables</title>
+    </head>
+
+    <body>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/includes/nav.php"; ?>
+
+            <div class="contentWrapper">
+                <div class="writtenWrapper">
+                    <h1>Reference tables</h1>
+                    <p>
+                        In this section are various useful tables and references.
+                    </p>
+                </div>
+            </div>
+
+            <div class="wrapper">
+                <div class="entries">
+
+                </div>
+            </div>
+    </body>
+
+    <?php include $_SERVER['DOCUMENT_ROOT'] . "/includes/footer.php"; ?>
+</html>
