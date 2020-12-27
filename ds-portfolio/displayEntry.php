017eeb5b13d1864f368114c6c6bbf9ba3155929f
dereckdemezquita <dereck@demezquita.com>
2020-12-27 02:50:43 -0800

    Data science portfolio.

---
+++ ds-portfolio/displayEntry.php
@@ -0,0 +1,30 @@
+<html lang="en-uk">
+    <head>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/includes/header.php"; ?>
+        <script type="text/javascript" src="/ds-portfolio/getEntries.js"></script>
+    </head>
+
+    <body>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/includes/nav.php"; ?>
+
+        <div class="contentWrapper">
+            <?php include $_SERVER['DOCUMENT_ROOT'] . "/includes/sidebar.php"; ?>
+
+            <article>
+                <?php
+                    
+                    $entry = $_GET["entry"];
+
+                    $docRoot = $_SERVER['DOCUMENT_ROOT'];
+
+                    $entryHTML = file_get_contents($docRoot . "/ds-portfolio/entries/" . $entry . ".html", FILE_USE_INCLUDE_PATH);
+                
+                    echo($entryHTML);
+
+                ?>
+            </article>
+        </div>
+    </body>
+    
+    <?php include $_SERVER['DOCUMENT_ROOT'] . "/includes/footer.php"; ?>
+</html>
