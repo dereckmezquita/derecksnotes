3e57d92cd47f12e92ba6b72467012d0ca60502a2
dereckdemezquita <dereck@demezquita.com>
2020-12-27 03:05:23 -0800

    Scientists

---
+++ references/scientists/scientists.php
@@ -0,0 +1,28 @@
+<html lang="en-uk">
+    <head>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/includes/header.php"; ?>
+        <script type="text/javascript" src="/references/scientists/getEntries.js"></script>
+        <title>Scientists</title>
+    </head>
+
+    <body>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/includes/nav.php"; ?>
+
+            <div class="contentWrapper">
+                <div class="writtenWrapper">
+                    <h1>Scientists</h1>
+                        <p>
+                            The legends the giants on which we stand, they all had families, friends, and lovers. Here is a short overview on some famous scientists, their contributions, and intimate lives.
+                        </p>
+                </div>
+            </div>
+
+            <div class="wrapper">
+                <div class="entries">
+
+                </div>
+            </div>
+    </body>
+
+    <?php include $_SERVER['DOCUMENT_ROOT'] . "/includes/footer.php"; ?>
+</html>
