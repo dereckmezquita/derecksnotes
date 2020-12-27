c5144803346b5d0433855149241d368852fcba60
dereckdemezquita <dereck@demezquita.com>
2020-12-27 03:28:09 -0800

    Art references.

---
+++ galleries/references/references.php
@@ -0,0 +1,28 @@
+<html lang="en-uk">
+    <head>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/includes/header.php"; ?>
+        <script type="text/javascript" src="/galleries/references/getEntries.js"></script>
+        <title>References</title>
+    </head>
+
+    <body>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/includes/nav.php"; ?>
+
+            <div class="contentWrapper">
+                <div class="writtenWrapper">
+                    <h1>References</h1>
+                        <p>
+                            In this section are a variety of refrences either to nature or just beautiful diagrams I've found inspiring. Not very organised but meant to be enjoyable rather than efficient.
+                        </p>
+                </div>
+            </div>
+
+            <div class="wrapper">
+                <div class="entries">
+
+                </div>
+            </div>
+    </body>
+
+    <?php include $_SERVER['DOCUMENT_ROOT'] . "/includes/footer.php"; ?>
+</html>
