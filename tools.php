78b36da35830d1b7a213c39355135d9891757d75
dereckdemezquita <dereck@demezquita.com>
2020-12-27 21:24:58 -0800

    Restructured.

---
+++ tools.php
@@ -0,0 +1,29 @@
+<html lang="en-uk">
+    <head>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/includes/header.php"; ?>
+        <script type="text/javascript" src="/tools/getEntries.js"></script>
+
+        <title>Tools</title>
+    </head>
+
+    <body>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/includes/nav.php"; ?>
+        
+            <div class="contentWrapper">
+                <div class="writtenWrapper">
+                    <h1>Tools</h1>
+                        <p>
+                            Here you will find a repertoire of different tools and programmes. All run in JavaScript and directly from the browser. Some will deal with DNA sequences, and others modelisation. These are rather time consuming and difficult to create so there are not very many of them.
+                        </p>
+                </div>
+            </div>
+
+            <div class="wrapper">
+                <div class="entries">
+
+                </div>
+            </div>
+    </body>
+
+    <?php include $_SERVER['DOCUMENT_ROOT'] . "/includes/footer.php"; ?>
+</html>
